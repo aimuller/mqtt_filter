5e3ae0261306e80c7ed282f79c8d43cfe93d03d1
aimuller <8037561+aimuller@user.noreply.gitee.com>
2021-03-15 20:26:48 +0800

    未编译过

---
+++ header.h
@@ -0,0 +1,65 @@
+#include <linux/kernel.h>  
+#include <linux/init.h>  
+#include <linux/module.h>  
+#include <linux/version.h>  
+#include <linux/string.h>  
+#include <linux/kmod.h>  
+#include <linux/vmalloc.h>  
+#include <linux/workqueue.h>  
+#include <linux/spinlock.h>  
+#include <linux/socket.h>  
+#include <linux/net.h>  
+#include <linux/in.h>
+#include <linux/skbuff.h>  
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <linux/icmp.h> 
+#include <linux/netfilter.h>  
+#include <linux/netfilter_ipv4.h>  
+#include <linux/icmp.h>  
+#include <linux/fs.h>
+#include <linux/if_arp.h>  
+#include <linux/cdev.h>
+#include <linux/types.h>
+#include <linux/errno.h>
+#include <linux/kdev_t.h>
+#include <net/sock.h>  
+#include <asm/uaccess.h>  
+#include <asm/unistd.h>
+#include <linux/kthread.h>
+#include <linux/list.h>
+#include <linux/string.h>
+#include <linux/timer.h>
+#include <linux/timex.h>
+#include <linux/rtc.h>
+
+/*14种类型的MQTT报文定义*/
+#define CONNECT 0x10U
+#define CCONNACK 0x20U
+#define PUBLISH 0x30U
+#define PUBACK 0x40U
+#define PUBREC 0x50U
+#define PUBREL 0x60U
+#define PUBCOMP 0x70U
+#define SUBSCRIBE 0x80U
+#define SUBACK 0x90U
+#define UNSUBSCRIBE 0xA0U
+#define UNSUBACK 0xB0U
+#define PINGREQ 0xC0U
+#define PINGRESP 0xD0U
+#define DISCONNECT 0xE0U
+#define AUTH 0xF0U
+
+/*其他常量定义*/
+#define MQTT_PORT 1883		/*MQTT服务端口*/
+#define UNKNOWN 0xFEFEFEFE
+#define ANY 0x3F3F3F3F
+#define ANY_PORT 0x3F3F
+#define PERMIT 0x1
+#define DENY 0x0
+#define OK 0x1
+#define ERR 0x0
+#define DEFAULT NF_ACCEPT  /*默认策略*/
+
+
---
+++ mqtt_filter_mod.c
@@ -0,0 +1,193 @@
+#include "header.h"
+
+static struct nf_hook_ops nfho[2];	/*nf_hook_ops结构声明*/
+static int active = 1;	/*active=1表示开启, active=0表示关闭*/
+
+struct RULER_ST{		/*通用规则结构定义*/
+	u_int32_t saddr;	/*源地址*/
+	u_int32_t smask;	/*目的地址*/
+	u_int32_t daddr;	/*源端口*/
+	u_int32_t dmask;	/*目的端口*/
+	u_int8_t type;		/*指MQTT报文的类型*/
+	u_int8_t log;		/*是否记录日志*/
+	u_int8_t action;	/*动作*/
+};
+
+struct RULER_LIST_ST{	/*规则链表定义，使用Linux内核提供的链表list*/
+	struct list_head list;	/*内核链表结构*/
+	struct RULER_ST ruler;		/*表示一条规则*/
+};
+
+struct RULER_LIST_ST rulers_head;	/*定义规则链表头结点*/
+unsigned int ruler_num;		/*目前的规则条数*/
+
+
+/*插入规则链表节点*/
+static int add_node(struct RULER_ST ruler, unsigned int N)
+{
+	struct RULER_LIST_ST *newNode;
+	struct list_head *pos;
+	int i;
+	
+	if(N <= 0 || N > ruler_num + 1){
+		printk("插入位置违法! \n");
+		return ERR;
+	}
+	
+	pos = &rulers_head.list;
+    for (i = 1; i < N; i++)	/*将节点插入第N个位置*/
+		pos = pos -> next;
+	
+	/*申请新的规则节点*/
+	newNode = (struct RULER_LIST_ST *)kmalloc(sizeof(struct RULER_LIST_ST), GFP_KERNEL);
+	newNode.ruler.saddr = ruler.saddr;
+	newNode.ruler.daddr = ruler.daddr;
+	newNode.ruler.type = ruler.type;
+	list_add_tail(&newNode->list, pos);
+	ruler_num++;
+	
+	return OK;
+}
+
+/*删除规则链表节点*/
+static int del_node(struct RULER_ST ruler, unsigned int N)
+{
+	struct RULER_LIST_ST *newNode;
+	struct list_head *pos;
+	int i;
+	
+	if(N <= 0 || N > ruler_num){
+		printk("删除位置违法! \n");
+		return ERR;
+	}
+	
+	if(list_empty(&rulers_head.list)){
+		printk("规则链表为空! \n");
+		return ERR;
+	}
+	
+	pos = &rulers_head.list;
+    for (i = 0; i < N; i++)	/*找到要删除节点的list指针*/
+		pos = pos -> next;
+	
+	list_del(pos);	/*删除list节点链表关系*/
+	p = list_entry(pos, struct RULER_LIST_ST, list);
+	kfree(p); 		/*释放该结点所占空间*/
+	ruler_num--;
+	
+	return OK;
+}
+
+/*mqtt_check函数*/
+static int mqtt_check(struct RULER_ST *ruler, char *mqtth){
+	u_int8_t *ptr = (u_int8_t *)mqtth;
+	
+	if(ruler->type == (*ptr)&ruler->type) /* MQTT报文类型定义的值恰好可以作为其"掩码" */
+		return YES;
+	
+	return NO;
+}
+
+/*ip_check函数*/
+static int ip_check(struct RULER_ST *ruler, struct iphdr *iph){
+	u_int32_t *saddr = (u_int32_t *)&iph->saddr;
+	u_int32_t *daddr = (u_int32_t *)&iph->daddr;
+
+	if( (ruler->saddr == ANY || (ruler->saddr & ruler->smask) == (*saddr & ruler->ruler)) &&
+		(ruler->daddr == ANY || (ruler->daddr & ruler->dmask) == (*daddr & ruler->dmask)) )
+		return YES;
+	//printk("src_ip: %x, %x\n", node->src_ip, *src_ip);
+	//printk("dest_ip: %x, %x\n", node->dest_ip, *dest_ip);
+	return NO;
+}
+
+/*规则check函数*/
+static unsigned int check(struct sk_buff *skb)
+{
+	struct iphdr *iph;
+	struct tcphdr *tcph;
+	char *mqtth;
+	struct list_head *tmp;
+	struct RULER_LIST_ST *node;
+	
+	iph = ip_hdr(skb);	/*获取IP头*/
+	if(iph -> protocol == IPPROTO_TCP){
+		tcph = tcp_hdr(skb);	/*获取TCP头*/
+		if(tcph -> dest == MQTT_PORT || tcph -> source == MQTT_PORT){
+			mqtth = (char *)tcph + tcph -> doff * 4;	/*获取MQTT头*/
+			list_for_each(tmp, &rulers_head.list) {
+				node = list_entry(tmp, struct RULER_LIST_ST, list);
+				if(ip_check(&node->ruler, iph) && mqtt_check(&node->ruler, mqtth)){
+					return node->ruler.action;
+				}
+			}
+			return DEFAULT; /*默认策略*/
+		}
+		return NF_ACCEPT;
+	}
+	return NF_ACCEPT;
+}
+
+
+/*Hook函数*/
+unsigned int mqtt_filter(void *priv,
+			struct sk_buff *skb,
+			const struct nf_hook_state *state)
+{
+	if(!active)
+		return NF_ACCEPT;
+	return check(skb);
+}
+
+
+/*mqtt过滤模块注册函数*/
+static int myfilter_init(void)
+{ 
+	/*初始化规则链表*/
+	ruler_num = 0;
+	INIT_LIST_HEAD(&rulers_head.list);
+
+	/* 在hook点注册相应的hook函数 */  
+	nfho[0].hook = mqtt_filter;
+	nfho[0].pf = PF_INET;
+	nfho[0].hooknum = NF_INET_PRE_ROUTING;
+	nfho[0].priority = NF_IP_PRI_FIRST; 
+
+	nfho[1].hook = mqtt_filter;
+	nfho[1].pf = PF_INET;
+	nfho[1].hooknum = NF_INET_POST_ROUTING;
+	nfho[1].priority = NF_IP_PRI_FIRST;
+
+	printk(KERN_INFO "正在注册MQTT过滤模块...\n");
+	nf_register_hook(&nfho[0]);
+	nf_register_hook(&nfho[1]);
+	printk(KERN_INFO "模块注册成功.\n");
+
+	return 0;
+}
+
+/*mqtt过滤模块注销函数*/
+static void myfilter_exit(void)
+{
+	struct RULER_LIST_ST *node;
+    struct list_head *pos, *tmp;
+	
+	/*清理规则链表，释放节点空间*/
+    list_for_each_safe(pos, tmp, &rulers_head.list) {
+        list_del(pos);
+        node = list_entry(pos, struct RULER_LIST_ST, llist);
+        kfree(node);
+    }
+	ruler_num = 0;
+	
+	printk(KERN_INFO "正在注销MQTT过滤模块...\n");
+	nf_unregister_hook(&nfho[0]);
+	nf_unregister_hook(&nfho[1]);
+	printk(KERN_INFO "模块注销成功.\n");
+}
+
+
+module_init(myfilter_init);
+module_exit(myfilter_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("lrz");
